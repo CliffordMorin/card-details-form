e077fccd38e09aa0aaaba58e9c837f2e517c0e28
Clifford Morin <cemorin21@gmail.com>
2023-01-20 15:01:53 -0500

    card number error check funstionalitiy

--- app/src/Components/Submit.js
+++ app/src/Components/Submit.js
@@ -1,7 +1,31 @@
-const Submit = (info, setInfo, setConfirm) => {
+import React, { useState } from "react";
+
+const Submit = ({ info, setInfo, setConfirm }) => {
+  const [cardNumberError, setCardNumberError] = useState({
+    color: "",
+    message: "",
+  });
+
   const checkCardNumber = (cardNumber) => {
     const cardNumberRegex = new RegExp("^[0-9]{16}$");
     return cardNumberRegex.test(cardNumber);
+  };
+
+  const submitCardNumber = (e) => {
+    const cardNumber = e.target.value;
+    if (!checkCardNumber(cardNumber)) {
+      setCardNumberError({
+        color: "red",
+        message: "Card number must be 16 digits and numbers only",
+      });
+    } else {
+      setCardNumberError({
+        color: "green",
+        message: "Card number is valid",
+      });
+      setInfo({ ...info, cardNumber: cardNumber });
+      console.log(info);
+    }
   };
 
   return (
@@ -10,7 +34,18 @@ const Submit = (info, setInfo, setConfirm) => {
         <label htmlFor="cardHolderName">CardHolder Name</label>
         <input type="text" name="cardHolderName" id="cardHolderName" required />
         <label htmlFor="cardNumber">Card Number</label>
-        <input type="text" name="cardNumber" id="cardNumber" required />
+        <input
+          type="text"
+          name="cardNumber"
+          id="cardNumber"
+          onChange={(e) => submitCardNumber(e)}
+          required
+        />
+        {cardNumberError.message && (
+          <p style={{ color: cardNumberError.color }}>
+            {cardNumberError.message}
+          </p>
+        )}
         <label htmlFor="expiryDate">Expiry Date (MM/YY)</label>
         <input type="text" name="expiryMonth" id="expiryMonth" required />
         <input type="text" name="expiryYear" id="expiryYear" required />
