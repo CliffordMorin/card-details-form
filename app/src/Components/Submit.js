ab0a75adf2052953f6201877626688c84645bf93
Clifford Morin <cemorin21@gmail.com>
2023-01-20 16:14:24 -0500

    trying to get card info to update to toggle disabled submit button

--- app/src/Components/Submit.js
+++ app/src/Components/Submit.js
@@ -6,9 +6,36 @@ const Submit = ({ info, setInfo, setConfirm }) => {
     message: "",
   });
 
+  const [inputBlank, setInputBlank] = useState({
+    color: "",
+    message: "",
+  });
+
+  const [submitValid, setSubmitValid] = useState(false);
+
   const checkCardNumber = (cardNumber) => {
     const cardNumberRegex = new RegExp("^[0-9]{16}$");
     return cardNumberRegex.test(cardNumber);
+  };
+
+  const isInputBlank = (input) => {
+    const inputRegex = new RegExp("^[a-zA-Z0-9]{1,}$");
+    return inputRegex.test(input);
+  };
+
+  const submitIsInputBlank = (e) => {
+    const input = e.target.value;
+    if (!isInputBlank(input)) {
+      setInputBlank({
+        color: "red",
+        message: "Can't be blank",
+      });
+    } else {
+      setInputBlank({
+        color: "green",
+        message: "",
+      });
+    }
   };
 
   const submitCardNumber = (e) => {
@@ -16,7 +43,7 @@ const Submit = ({ info, setInfo, setConfirm }) => {
     if (!checkCardNumber(cardNumber)) {
       setCardNumberError({
         color: "red",
-        message: "Card number must be 16 digits and numbers only",
+        message: "Wrong format, numbers only",
       });
     } else {
       setCardNumberError({
@@ -28,30 +55,128 @@ const Submit = ({ info, setInfo, setConfirm }) => {
     }
   };
 
+  const checkFormValidity = () => {
+    setSubmitValid(
+      [info.cardHolderName, info.cardNumber, info.expiryDate, info.cvv].every(
+        (input) => input.trim().length > 0
+      )
+    );
+  };
+
+  const setExpiryDate = (e) => {
+    setInfo({
+      ...info,
+      expiryDate: e.target.expiryMonth.value + "/" + e.target.expiryYear.value,
+    });
+  };
+
+  const formSubmit = (e) => {
+    e.preventDefault();
+    if (checkFormValidity()) {
+      alert("Please fill in all fields");
+    } else {
+      setInfo({
+        cardHolderName: e.target.cardHolderName.value,
+        cardNumber: e.target.cardNumber.value,
+        expiryDate:
+          e.target.expiryMonth.value + "/" + e.target.expiryYear.value,
+        cvv: e.target.cvv.value,
+      });
+      setConfirm(true);
+    }
+  };
+
   return (
     <div>
       <form action="">
         <label htmlFor="cardHolderName">CardHolder Name</label>
-        <input type="text" name="cardHolderName" id="cardHolderName" required />
+        <input
+          type="text"
+          name="cardHolderName"
+          id="cardHolderName"
+          style={{ borderColor: inputBlank.color }}
+          onChange={(e) => {
+            submitIsInputBlank(e);
+            setInfo(info.cardHolderName);
+          }}
+          required
+        />
+        {inputBlank.message && (
+          <p style={{ color: inputBlank.color }}>{inputBlank.message}</p>
+        )}
         <label htmlFor="cardNumber">Card Number</label>
         <input
           type="text"
           name="cardNumber"
           id="cardNumber"
+          style={{ borderColor: cardNumberError.color }}
           onChange={(e) => submitCardNumber(e)}
+          maxLength="16"
           required
         />
         {cardNumberError.message && (
-          <p style={{ color: cardNumberError.color }}>
+          <p
+            style={{
+              color: cardNumberError.color,
+            }}
+          >
             {cardNumberError.message}
           </p>
         )}
         <label htmlFor="expiryDate">Expiry Date (MM/YY)</label>
-        <input type="text" name="expiryMonth" id="expiryMonth" required />
-        <input type="text" name="expiryYear" id="expiryYear" required />
-        <label htmlFor="cvc">CVC</label>
-        <input type="text" name="cvc" id="cvc" required />
-        <button type="submit">Confirm</button>
+        <input
+          type="text"
+          name="expiryMonth"
+          id="expiryMonth"
+          maxLength="2"
+          placeholder="MM"
+          style={{ borderColor: inputBlank.color }}
+          onChange={(e) => {
+            submitIsInputBlank(e);
+            setExpiryDate(e);
+          }}
+          required
+        />
+        {inputBlank.message && (
+          <p style={{ color: inputBlank.color }}>{inputBlank.message}</p>
+        )}
+        <input
+          type="text"
+          name="expiryYear"
+          id="expiryYear"
+          maxLength="2"
+          placeholder="YY"
+          style={{ borderColor: inputBlank.color }}
+          onChange={(e) => {
+            submitIsInputBlank(e);
+            setExpiryDate(e);
+          }}
+          required
+        />
+        <label htmlFor="cvv">CVV</label>
+        <input
+          type="text"
+          name="cvv"
+          id="cvv"
+          maxLength="3"
+          placeholder="e.g. 123"
+          style={{ borderColor: inputBlank.color }}
+          onChange={(e) => {
+            submitIsInputBlank(e);
+            setInfo(info.cvv);
+          }}
+          required
+        />
+        {inputBlank.message && (
+          <p style={{ color: inputBlank.color }}>{inputBlank.message}</p>
+        )}
+        <button
+          type="submit"
+          onSubmit={(e) => formSubmit(e)}
+          disabled={!submitValid}
+        >
+          Confirm
+        </button>
       </form>
     </div>
   );
